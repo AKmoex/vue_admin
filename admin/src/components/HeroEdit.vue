2e29dc568e381f9420758de3efb03f3c990a6508
iStormDog <istormlala@gmail.com>
2020-04-06 22:30:34 +0800

    add skills_icon

--- admin/src/components/HeroEdit.vue
+++ admin/src/components/HeroEdit.vue
@@ -78,7 +78,9 @@
                                     class="avatar-uploader"
                                         :action="$http.defaults.baseURL+'/upload'"
                                         :show-file-list="false"
-                                        :on-success="afterUpload">
+                                        :on-success="(res)=>{
+                                            $set(item,'icon',res.url)
+                                        }">
                                     <img v-if="item.icon" :src="item.icon" class="avatar">
                                     <i v-else class="el-icon-plus avatar-uploader-icon"></i>
                                 </el-upload>
