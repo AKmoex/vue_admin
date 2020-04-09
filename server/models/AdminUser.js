0ed962776457d6623b5253ecfdbf5872f82497ad
iStormDog <istormlala@gmail.com>
2020-04-09 20:17:06 +0800

    bcrypt加密
    npm i bcryptjs -S

--- server/models/AdminUser.js
+++ server/models/AdminUser.js
@@ -6,7 +6,10 @@ const adminUserSchema=new Schema({
     username:{type:String},
     password:{
         type:String,
-      
+        select:false,   //防止对散列后的值再次进行散列
+        set(val){
+            return require('bcryptjs').hashSync(val,10)
+        }
     }
 })
 module.exports=mongoose.model('AdminUser',adminUserSchema)
