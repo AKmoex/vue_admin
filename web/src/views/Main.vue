5b6ce94842b68c72ca2ee8f391494be8358fcdc0
iStormDog <istormlala@gmail.com>
2020-04-17 21:16:16 +0800

    tab

---
+++ web/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="topbar bg-black text-whilte py-2 px-3 d-flex ai-center">
+      <img src="../assets/logo.png" height="30" />
+      <div class="px-3">
+        <div class="text-white">王者荣耀</div>
+        <div class="text-grey-1 fs-xss">团队成就更多</div>
+      </div>
+      <button class="btn bg-primary">立即下载</button>
+    </div>
+    <div class="nav">nav</div>
+
+    <router-view></router-view>
+  </div>
+</template>
